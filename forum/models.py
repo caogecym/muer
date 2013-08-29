d6b06943942234652448fc01795d91ed6f8cb66d
Yuming Cao <caogecym@gmail.com>
2013-08-28 22:48:59 -0700

    gitignore now worked/add model test

--- forum/models.py
+++ forum/models.py
@@ -7,14 +7,14 @@ import datetime
 #from libs.models.fields import ListField
 
 class Tag(models.Model):
-    name            = models.CharField(max_length=255, unique=True)
-    author          = models.ForeignKey(User, related_name='created_tags')
-    deleted         = models.BooleanField(default=False)
-    deleted_at      = models.DateTimeField(null=True, blank=True)
-    deleted_by      = models.ForeignKey(User, null=True, blank=True, related_name='deleted_tags')
+    name                = models.CharField(max_length=255, unique=True)
+    author              = models.ForeignKey(User, null=True, blank=True, related_name='created_tags')
+    deleted             = models.BooleanField(default=False)
+    deleted_at          = models.DateTimeField(null=True, blank=True)
+    deleted_by          = models.ForeignKey(User, null=True, blank=True, related_name='deleted_tags')
 
     # Denormalised data
-    used_count = models.PositiveIntegerField(default=0)
+    used_count          = models.PositiveIntegerField(default=0)
     
     # TODO: We need to figure this out
     #objects = TagManager()
@@ -25,51 +25,59 @@ class Tag(models.Model):
     def __unicode__(self):
         return self.name
 
+class Image(models.Model):
+    local_image_src     = models.CharField(max_length=1024)
+    remote_image_src    = models.CharField(max_length=1024)
+
+    class Meta:
+        db_table = u'image'
+
 class Comment(models.Model):
-    content_type   = models.ForeignKey(ContentType)
-    object_id      = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
-    author         = models.ForeignKey(User, related_name='comments')
-    content        = models.CharField(max_length=300)
-    image_src      = models.CharField(max_length=300)
+    content_type        = models.ForeignKey(ContentType)
+    object_id           = models.PositiveIntegerField()
+    content_object      = generic.GenericForeignKey('content_type', 'object_id')
+    content             = models.CharField(max_length=1024)
+    images              = generic.GenericRelation(Image)
+    author              = models.ForeignKey(User, related_name='comments')
 
     # status
-    added_at       = models.DateTimeField(default=datetime.datetime.now)
-    deleted        = models.BooleanField(default=False)
-    deleted_at     = models.DateTimeField(null=True, blank=True)
-    deleted_by     = models.ForeignKey(User, null=True, blank=True, related_name='deleted_comments')
+    added_at            = models.DateTimeField(default=datetime.datetime.now)
+    deleted             = models.BooleanField(default=False)
+    deleted_at          = models.DateTimeField(null=True, blank=True)
+    deleted_by          = models.ForeignKey(User, null=True, blank=True, related_name='deleted_comments')
 
     # user preference 
-    liked_by       = generic.GenericRelation(User)
-    like_count     = models.PositiveIntegerField(default=0)
+    liked_by            = models.ManyToManyField(User, null=True, blank=True, related_name='liked_comments')
+    like_count          = models.PositiveIntegerField(default=0)
+    comments            = generic.GenericRelation('self')
+    comment_count       = models.PositiveIntegerField(default=0)
 
     class Meta:
         ordering = ('-added_at',)
         db_table = u'comment'
+
     def __unicode__(self):
-        return self.comment
+        return self.content
 
 class Post(models.Model):
-    title       = models.CharField(max_length=300, unique=True)
-    author      = models.ForeignKey(User, related_name='posts')
-    post_source = models.CharField(max_length=1024, null=True, blank=True)
-    # TODO: how to store image
-    content     = models.CharField(max_length=1024)
-    image_src   = models.CharField(max_length=300, null=True, blank=True)
-    tags        = models.ManyToManyField(Tag, null=True, blank=True, related_name='posts')
+    title               = models.CharField(max_length=300, unique=True)
+    author              = models.ForeignKey(User, related_name='posts')
+    post_source         = models.CharField(max_length=1024)
+    content             = models.CharField(max_length=1024)
+    images              = generic.GenericRelation(Image)
+    tags                = models.ManyToManyField(Tag, null=True, blank=True, related_name='tagged_posts')
 
     # status
-    added_at    = models.DateTimeField(default=datetime.datetime.now)
-    deleted     = models.BooleanField(default=False)
-    deleted_at  = models.DateTimeField(null=True, blank=True)
-    deleted_by  = models.ForeignKey(User, null=True, blank=True, related_name='deleted_posts')
+    added_at            = models.DateTimeField(default=datetime.datetime.now)
+    deleted             = models.BooleanField(default=False)
+    deleted_at          = models.DateTimeField(null=True, blank=True)
+    deleted_by          = models.ForeignKey(User, null=True, blank=True, related_name='deleted_posts')
 
     # user preference 
-    liked_by    = generic.GenericRelation(User)
-    comments    = generic.GenericRelation(Comment)
-    view_count  = models.PositiveIntegerField(default=0)
-    like_count  = models.PositiveIntegerField(default=0)
-    comment_count = models.PositiveIntegerField(default=0)
+    liked_by            = models.ManyToManyField(User, null=True, blank=True, related_name='liked_posts')
+    like_count          = models.PositiveIntegerField(default=0)
+    comments            = generic.GenericRelation(Comment)
+    comment_count       = models.PositiveIntegerField(default=0)
 
     def __unicode__(self):
         return self.content
@@ -78,4 +86,3 @@ class Post(models.Model):
 
 # custom signal
 user_logged_in = django.dispatch.Signal(providing_args=["session"])
-
--- forum/tests.py
+++ forum/tests.py
@@ -11,17 +11,52 @@ from forum.models import Post, Tag, Comment
 class PostTestCase(TestCase):
     def setUp(self):
         user = User(username='caogecym')
-        tag1 = Tag(name='cold', author=user)
+        user1 = User(username='caogecym1')
+        user2 = User(username='caogecym2')
+        user.save()
+        user1.save()
+        user2.save()
+        Post.objects.create(title="test_post", content="Ultimate anwser to everything: 42", author=user)
+
+    def test_tag(self):
+        post = Post.objects.get(title='test_post')
+        user = post.author
+        # tag1 no author
+        tag1 = Tag(name='cold')
+        tag1.save()
+        # tag2 with author
         tag2 = Tag(name='science', author=user)
-        post = Post(title="test_post", content="Ultimate anwser to everything: 42", author=user)
+        tag2.save()
+        post.tags.add(tag1)
+        post.tags.add(tag2)
+        self.assertEqual(len(post.tags.all()), 2)
 
-    def test_get_post_content(self):
-        """test get post content"""
-        coldJoke = Post.objects.get(title="cold_joke")
-        self.assertEqual(coldJoke.tags, ['cold'])
+    def test_comment(self):
+        post = Post.objects.get(title='test_post')
+        user = post.author
 
+        # two comments for post
+        comment0 = Comment(content_object=post, author=user, content='I am comment 1')
+        comment0.save()
+        comment1 = Comment(content_object=post, author=user, content='I am comment 2')
+        comment1.save()
+        self.assertEqual(len(post.comments.all()), 2)
+
+        # recursive comment for comment0
+        comment0_0 = Comment(content_object=comment0, author=user, content='I am comment 0 for comment 1')
+        comment0_0.save()
+        self.assertEqual(len(post.comments.all().filter(content='I am comment 1')[0].comments.all()), 1)
+
+    def test_liked_by(self):
+        post = Post.objects.get(title='test_post')
+        user1 = User.objects.all().filter(username='caogecym1')[0]
+        user2 = User.objects.all().filter(username='caogecym2')[0]
+        post.liked_by.add(user1)
+        post.liked_by.add(user2)
+        self.assertEqual(len(post.liked_by.all()), 2)
+        
     def test_post_duplicate_check(self):
         """test get post content"""
         # create duplicate one
-        self.assertRaises(IntegrityError, Post.objects.create, title="cold_joke", content="haha", \
-                          tags=['cold', 'duplicate'])
+        user = User.objects.filter(username='caogecym')[0]
+        self.assertRaises(IntegrityError, Post.objects.create, title="test_post", content="haha", author=user)
