cc5682c1d30c36f4a24d1bb28b15a8b7c8e5547f
ycao <ycao@ycao-lt.(none)>
2013-10-01 22:13:41 -0700

    add install script

---
+++ utils/install.sh
@@ -0,0 +1,3 @@
+sudo aptitude install vim git cscope python-setuptools python-pip libxml2-dev libxslt-dev python-dev lib32z1-dev
+sudo pip install Django markdown requests lxml beautifulsoup4
+sudo easy_install South
