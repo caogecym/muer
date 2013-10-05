a6bc56558d7f3024420d216bb748435fde9f44c7
ycao <caogecym@gmail.com>
2013-10-05 13:48:12 -0700

    db setting production

--- muer/settings_production.py
+++ muer/settings_production.py
@@ -30,6 +30,11 @@ MANAGERS = ADMINS
 #    },
 #}
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
+
 # Registration regulation
 MIN_USERNAME_LENGTH = 4
 EMAIL_UNIQUE = True
@@ -177,9 +182,6 @@ LOGGING = {
     }
 }
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
