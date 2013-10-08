34898db299f864219bffb1c5d9c3dd62be185774
ycao <caogecym@gmail.com>
2013-10-07 23:33:19 -0700

    add stage setting

--- muer/settings_stage.py
+++ muer/settings_stage.py
@@ -13,6 +13,7 @@ ADMINS = (
     ('Yuming Cao', 'caogecym@gmail.com'),
 )
 
+SEND_BROKEN_LINK_EMAILS = True
 MANAGERS = ADMINS
 
 DATABASES = {
@@ -35,6 +36,7 @@ EMAIL_UNIQUE = True
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+# Allow all host headers
 ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
