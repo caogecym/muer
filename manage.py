089a6f502ebe195a55443c09e5aa6b2b6055c950
Yuming Cao <caogecym@gmail.com>
2014-04-19 17:27:16 +0000

    update manage.py

--- manage.py
+++ manage.py
@@ -3,9 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "muer.settings_dev")
+    if os.environ.get('DEV_MODE'):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "muer.settings_dev")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "muer.settings_production")
+
     #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "muer.settings_stage")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "muer.settings_production")
 
     from django.core.management import execute_from_command_line
 
