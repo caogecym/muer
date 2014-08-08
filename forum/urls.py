1d565a156729e79c01cf07180729a0546ca971c3
Yuming Cao <caogecym@gmail.com>
2014-08-08 05:33:41 +0000

    combine api with normal view url

--- forum/urls.py
+++ forum/urls.py
@@ -1,36 +1,21 @@
 from django.conf.urls import patterns, url, include
-from rest_framework.routers import DefaultRouter
-from forum.api import views
+from forum import views
 
-# Create a router and register our viewsets with it.
-router = DefaultRouter()
-router.register(r'posts', views.PostViewSet)
-router.register(r'tags', views.TagViewSet)
-router.register(r'comments', views.CommentViewSet)
-router.register(r'users', views.UserViewSet)
-
-# The API URLs are now determined automatically by the router.
 urlpatterns = patterns('',
-    url(r'^', include(router.urls)),
-)
+    # ex: /posts
+    url(r'^$', views.index, name='index'),
+    # ex: /posts/hottest
+    url(r'hottest$', views.index_hottest, name='hottest_posts'),
+    # ex: /posts/new_post/
+    url(r'new_post/$', views.add_post, name='add_post'),
+    # ex: /posts/11/edit/
+    url(r'^(?P<post_id>\d+)/edit/$', views.add_post, name='edit_post'),
 
-#urlpatterns = patterns('',
-#    # ex: /posts
-#    url(r'^$', views.index, name='index'),
-#    # ex: /posts/hottest
-#    url(r'hottest$', views.index_hottest, name='hottest_posts'),
-#    # ex: /posts/new_post/
-#    url(r'new_post/$', views.add_post, name='add_post'),
-#    # ex: /posts/11/edit/
-#    url(r'^(?P<post_id>\d+)/edit/$', views.add_post, name='edit_post'),
-#
-#    url(r'^(?P<post_id>\d+)/submit-edit/$', views.add_post, name='submit-edit'),
-#    # ex: /posts/5/
-#    url(r'^(?P<post_id>\d+)/$', views.content, name='content'),
-#    # ex: /posts/17/like
-#    url(r'^(?P<post_id>\d+)/like/$', views.like, name='like'),
-#    # ex: /posts/17/delete
-#    url(r'^(?P<post_id>\d+)/delete/$', views.delete_post, name='delete_post'),
-#    # ex: /posts/17/comment
-#    url(r'^(?P<post_id>\d+)/comment/$', views.comment_post, name='comment_post'),
-#)
+    url(r'^(?P<post_id>\d+)/submit-edit/$', views.add_post, name='submit-edit'),
+
+    # ex: /posts/5/
+    url(r'^(?P<post_id>\d+)/$', views.content, name='content'),
+
+    # ex: /posts/17/comment
+    url(r'^(?P<post_id>\d+)/comment/$', views.comment_post, name='comment_post'),
+)
--- muer/urls.py
+++ muer/urls.py
@@ -2,21 +2,32 @@ import os.path
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
 from django.contrib import admin
+
+from rest_framework.routers import DefaultRouter
+
 from forum import views as forum_views
 from forum import urls as forum_urls
+from forum.api import views as forum_api
 
 admin.autodiscover()
 APP_PATH = os.path.dirname(__file__)
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'posts', forum_api.PostViewSet)
+router.register(r'tags', forum_api.TagViewSet)
+router.register(r'comments', forum_api.CommentViewSet)
+router.register(r'users', forum_api.UserViewSet)
+
 urlpatterns = patterns('',
     (r'^$', forum_views.index),
-    (r'^api/', include(forum_urls)),
+    (r'^api/', include(router.urls)),
     (r'^about/', forum_views.about),
     (r'^home/', forum_views.index),
-    #(r'^posts/', include(forum_urls, namespace="forum")),
-    #(r'^search/', forum_views.search),
-    #(r'^tags/$', forum_views.tags),
-    #(r'^tags/(?P<tag>[^/]+)/$', forum_views.tag),
+    (r'^posts/', include(forum_urls, namespace="forum")),
+    (r'^search/', forum_views.search),
+    (r'^tags/$', forum_views.tags),
+    (r'^tags/(?P<tag>[^/]+)/$', forum_views.tag),
     (r'^login/$', login),
     (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page':'/'}),
     (r'^signup/$', 'forum.views.register'),
